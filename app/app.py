c17741c10a5e48900b0cc5b0f6f58a42d2eff511
Thomas Zachary <thomaszachary06@proton.me>
2025-02-01 13:44:11 +0000

    added test route

--- app/app.py
+++ app/app.py
@@ -4,6 +4,10 @@ import flask
 import flask_cors
 
 
+from datetime import date
+from core.timeline_node import TimelineNode
+
+
 ichack25_app = flask.Flask(__name__, static_folder=None)
 ichack25_app.url_map.strict_slashes = False
 cors = flask_cors.CORS(ichack25_app, supports_credentials=True)
@@ -11,8 +15,20 @@ cors = flask_cors.CORS(ichack25_app, supports_credentials=True)
 
 @ichack25_app.route('/')
 def index():
-    return "Hello Dev!"
+    return "Hello World!"
 
+@ichack25_app.route('/test')
+def test():
+    node = TimelineNode(
+        [],
+        [],
+        "Tom's Life",
+        date(2006, 2, 24),
+        date(2026, 6, 25),
+        []
+    ) 
+    logging.info(node.to_json())
+    return ""
 
 logging.info(
     f":: Available app routes ::\n{ichack25_app.url_map}"
--- core/timeline_node.py
+++ core/timeline_node.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from core.model.data_model import DataModel
 from datetime import date
 
