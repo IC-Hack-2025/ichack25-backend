900d37d406687104668fdd99df2a83f97802828e
Emil Filip <emil.filip.dev@gmail.com>
2025-02-01 16:42:13 +0000

    Add dummy query creation endpoint

--- app/app.py
+++ app/app.py
@@ -6,8 +6,10 @@ import flask_socketio
 
 from datetime import date
 
+from ai import query_openai
 from core.model.timeline import Timeline
 from core.model.timeline_node import TimelineNode
+
 
 ichack25_app = flask.Flask(__name__, static_folder=None)
 ichack25_app.url_map.strict_slashes = False
@@ -17,7 +19,7 @@ socketio = flask_socketio.SocketIO(ichack25_app)
 
 @ichack25_app.route('/')
 def index():
-    return "Hello World!"
+    return flask.render_template('debug.html')
 
 
 @socketio.on("connect")
@@ -46,6 +48,26 @@ def get_event(event: str):
         )
     )
     return t.to_json()
+
+
+@ichack25_app.route('/query/create', methods=['POST'], endpoint="query_create")
+def query_create():
+    if flask.request.is_json:
+        data = flask.request.get_json()
+    else:
+        data = flask.request.form.to_dict()
+    if data is None or type(data) is not dict:
+        flask.abort(400)
+    query_text = data.get('query', None)
+    if query_text is None:
+        flask.abort(400)
+
+    logging.info(f"Creating an event query: {query_text}")
+    result = query_openai(
+        f"Tell me the events that were directly caused by or indirectly influenced by '{query_text}'. "
+        f"List just their names and dates, no descriptions. Don't say any other text."
+    )
+    return result
 
 
 logging.info(
