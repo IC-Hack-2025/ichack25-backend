673cc293b6a831965a76e1e093ba2b46e9d04854
Emil Filip <emil.filip.dev@gmail.com>
2025-02-01 16:52:37 +0000

    Fixes

--- ai.py
+++ ai.py
@@ -2,10 +2,12 @@ from datetime import date
 from typing import List, Optional
 from pydantic import BaseModel
 from openai import OpenAI
-from core.timeline_node import ConnectionType, TimelineConnection, TimelineNode
+from core.model.timeline_node import ConnectionType, TimelineConnection, TimelineNode
+
 
 EVENT_GRAPH_DEPTH: int = 3
 client = OpenAI()
+
 
 def get_response(content: str, responseType: BaseModel):
     return client.beta.chat.completions.parse(
@@ -15,7 +17,8 @@ def get_response(content: str, responseType: BaseModel):
             ],
             response_format=responseType,
         ).choices[0].message.parsed
-    
+
+
 class EventBaseResponse(BaseModel):
     date_start: date
     date_end: date
@@ -23,7 +26,7 @@ class EventBaseResponse(BaseModel):
     desc: str
     causedEvents: List[str]
     influencedEvents: List[str]
-    
+
 class MisconceptionResponse(BaseModel):
     misconceptions: List[str]
     debunkingArguments: List[str]
@@ -33,7 +36,7 @@ class EventGraph:
     def __init__(self, start_event: str, depth: int = EVENT_GRAPH_DEPTH):
         self.start = start_event
         self.depth = depth
-    
+
     def get(self, event_name: Optional[str] = None, curr_depth: int = 0) -> iter[TimelineNode | TimelineConnection]:
         if event_name is None:
             event_name = self.start
@@ -66,12 +69,14 @@ class EventGraph:
                     edge.connection_type = ConnectionType.INFLUENCED
 
     def get_base_info(self, event_name: str) -> EventBaseResponse:
-        return get_response(f"Describe {event_name} in a short paragraph. Also give the names of any events it caused, and any events it influenced.", EventBaseResponse)    
-    
+        return get_response(f"Describe {event_name} in a short paragraph. Also give the names of any events it caused, and any events it influenced.", EventBaseResponse)
     def get_misconceptions(self, event_name: str) -> MisconceptionResponse:
         return get_response(f"Give some common misconceptions about {event_name} and their debunking arguments.")
 
 
-g = EventGraph("The Battle of Hastings")
-for out in g.get():
-    print(out)
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv(".env.local")
+    g = EventGraph("The Battle of Hastings")
+    for out in g.get():
+        print(out)
