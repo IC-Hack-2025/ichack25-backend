e80566a8ca55bca3220173cb348157538127c574
Emil Filip <emil.filip.dev@gmail.com>
2025-02-01 14:19:03 +0000

    Fix

--- app/app.py
+++ app/app.py
@@ -3,10 +3,8 @@ import logging
 import flask
 import flask_cors
 
-
 from datetime import date
 from core.timeline_node import TimelineNode
-
 
 ichack25_app = flask.Flask(__name__, static_folder=None)
 ichack25_app.url_map.strict_slashes = False
@@ -17,18 +15,16 @@ cors = flask_cors.CORS(ichack25_app, supports_credentials=True)
 def index():
     return "Hello World!"
 
+
 @ichack25_app.route('/test')
 def test():
     node = TimelineNode(
-        [],
-        [],
-        "Tom's Life",
-        date(2006, 2, 24),
-        date(2026, 6, 25),
-        []
-    ) 
-    logging.info(node.to_json())
-    return ""
+        heading="Tom's Life",
+        date_start=date(2006, 2, 24),
+        date_end=date(2026, 6, 25),
+    )
+    return node.to_json()
+
 
 logging.info(
     f":: Available app routes ::\n{ichack25_app.url_map}"
--- core/timeline_node.py
+++ core/timeline_node.py
@@ -1,51 +1,37 @@
-from pydantic import BaseModel
+import aenum
+from pydantic import Field
 from core.model.data_model import DataModel
 from datetime import date
 
+
 current_id = 0
+
 
 def next_id() -> int:
     global current_id
     current_id += 1
     return current_id
 
-class TimelineContent(DataModel):
-    _VALID_KINDS = ["video", "text", "url", "citation"]
 
-    kind: str
+class ContentType(aenum.StrEnum):
+    TEXT = "text"
+    VIDEO = "video"
+    URL = "url"
+    CITATION = "citation"
+
+
+class TimelineContent(DataModel):
+    content_type: ContentType = Field(ContentType.TEXT)
     content: str
 
-    def __init__(self, kind: str, content: str):
-        if kind not in self._VALID_KINDS:
-            raise ValueError("TimelineContent was initialised with invalid kind!")
-    
-        self.kind = kind
-        self.content = content
 
 class TimelineNode(DataModel):
-    node_id: int
-    parents: list[int]
-    children: list[int]
+    node_id: int = Field(default_factory=next_id, frozen=True)
+    parents: list[int] = Field(default_factory=list)
+    children: list[int] = Field(default_factory=list)
+
     heading: str
     date_start: date
     date_end: date
-    contents: list[TimelineContent]
 
-    def __init__(
-        self,
-        parents: list[int],
-        children: list[int],
-        heading: str,
-        date_start: date,
-        date_end: date,
-        contents: list[TimelineContent]
-    ):
-        self.node_id = next_id()
-
-        self.parents = parents
-        self.children = children
-        self.heading = heading
-        self.date_start = date_start
-        self.date_end = date_end
-        self.contents = contents
-  
+    contents: list[TimelineContent] = Field(default_factory=list)
