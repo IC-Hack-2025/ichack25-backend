a20f8e1e440d0b9f21fcb748ec9a8f7f909a3ed2
Thomas Zachary <thomaszachary06@proton.me>
2025-02-02 03:14:39 +0000

    removed redundant endpoints

--- app/app.py
+++ app/app.py
@@ -3,9 +3,6 @@ import logging
 import flask
 import flask_cors
 import flask_socketio
-from datetime import date
-from core.model.timeline import Timeline
-from core.model.timeline_node import TimelineNode
 from core.process.timeline_generator import TimelineGenerator
 from app.session import SessionHandler
 
@@ -47,43 +44,6 @@ def handle_extend_timeline(node_id: int):
 @ichack25_app.route('/')
 def index():
     return flask.render_template('debug.html')
-
-
-@ichack25_app.route('/after/<event>', methods=['GET'], )
-def get_event(event: str):
-    logging.info(f"fetching information about '{event}'")
-
-    t = Timeline(
-        heading="This is a timeline",
-        description="This is a timeline description"
-    )
-    t.add_node(
-        TimelineNode(
-            heading="Placeholder",
-            date_start=date(1970, 1, 1),
-            date_end=date(1970, 1, 1)
-        )
-    )
-    return t.to_json()
-
-
-@ichack25_app.route('/query/create', methods=['POST'], endpoint="query_create")
-def query_create():
-    if flask.request.is_json:
-        data = flask.request.get_json()
-    else:
-        data = flask.request.form.to_dict()
-    if data is None or type(data) is not dict:
-        flask.abort(400)
-    query_text = data.get('query', None)
-    if query_text is None:
-        flask.abort(400)
-
-    logging.info(f"Creating an event query: {query_text}")
-    tg = TimelineGenerator()
-    timeline = tg.generate_timeline(query_text)
-    _ = [_ for _ in tg.continue_timeline(timeline)]
-    return timeline.to_json()
 
 
 logging.info(
